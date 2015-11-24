3762b0397144d2448e8ebd15f774660b23f4c2e6
Shallom_Onoja <shallomonoja@gmail.com>
2015-11-24 12:04:43 -0600

    update

---
+++ src/locator/src/segment.cpp
@@ -0,0 +1,13 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include <segment.h>
+
+int main(int argc, char **argv) {
+  
+  printf("got %d components\n", numberOfComponents(argv[1]));
+  printf("done! uff...thats hard work.\n");
+
+  return 0;
+}
+
